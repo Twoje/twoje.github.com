c74cf158b8a29acf3f05876ce27121f3a328e6fa
Luke Mommaerts <lmommaerts@gmail.com>
2012-10-28 19:25:16 -0700

    Large overhaul

--- script.js
+++ script.js
@@ -1,15 +1,18 @@
 $(document).ready(function() {
-	$("#colors a").click(function() {
-		$("body").css('background', $(this).css('background-color'));
-	})
+	$("#navbar a").click(function() {
+		var buttonURL = String(this);
+		var page = buttonURL.substr(buttonURL.indexOf('#'));
+		bgColor(this);
+		tabShadow(this);
+		hidePages(page);
+	});
+
 
 	function resize() {
 		if($(window).height() >= 600)
 		{
 			$("#page").height($(window).height() - ($("#bottombar").height() + $("#topbar").height()));
 		}
-		$("#footClamp").css("padding-bottom", ($(window).height()/2));
-		$("#headClamp").css("padding-top", ($(window).height()/2));
 	}
 
 	resize();
@@ -17,65 +20,31 @@ $(document).ready(function() {
 		resize();
 	})
 
-	var pages = ["#home", "#portfolio", "#experiments", "#other"];
-
-	function clamp(page) {
-		$(".clamp").slideDown('slow', function() {
-			hidePages(page); 
-		}).delay(200);
-		$(".clamp").slideUp('slow', function() {
-		});
-
-	}
+	var pages = ["#home", "#portfolio", "#experiments", "#other", "#contact"];
 
 	function hidePages(page) {
 		$("#home").hide();
 		$("#portfolio").hide();
 		$("#experiments").hide();
 		$("#other").hide();
+		$("#contact").hide();
 		$(page).show();
 	}
 
-	function arrow(page) {
-		$("#arrow").animate({"left" : $(page).position()}, "slow");
+	function bgColor(page) {
+		var color = $(page).css('background-color');
+		$("body").css('background', color);
+		$("body").css('box-shadow', '0px -1px 0px');
+	}
+
+	function tabShadow(page) {
+		$(page).css('box-shadow', '-3px 0px 5px -3px #111, inset 0 0 0 #111');
+		$("#navbar").css('box-shadow', 'inset 2px -2px 5px -2px #111');
 	}
 
 	/* Check if on homepage on load */
 	var docURL = String(document.URL);
 	var page = docURL.substr(docURL.indexOf('#'));
 	hidePages(page);
-	clamp(page);
 	$("#contact").hide();
-
-	/* Hide other pages when given page button clicked */
-	$("a[href^='#']").click(function() {
-		var buttonURL = String(this);
-		var page = buttonURL.substr(buttonURL.indexOf('#'));
-		if ($.inArray(page, pages) !== -1) {
-			clamp(page);
-			arrow("#contact");
-		}
-	})
-
-	/* Show contact form */
-	$("#navbar .contact").click(function() {
-		if($("#contact").is(":visible")) {
-			$("#contact").hide('slide', {direction: 'up'}, 'fast');
-		}
-		else {
-			$("#contact").show('slide', {direction: 'up'}, 'fast');
-			$("#contact").effect('bounce', {times: 1, direction: 'down', distance: 20}, 300);
-		}
-	});
-
-	/* Hide contact form when user clicks outside it */
-	$(document).mousedown(function (e) {
-		var container = $("#contact");
-
-		if(container.has(e.target).length === 0)
-		{
-			$("#contact").hide('slide', {direction: 'up'}, 'slow');
-		}
-	})
-	
 })
