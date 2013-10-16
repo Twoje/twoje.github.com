0e0244040fedb2012ddf4e8d5bf975cad4a3cd05
Luke Mommaerts <lmommaerts@gmail.com>
2013-10-16 15:51:23 -0400

    Added steam categorizer preview with slideshow for projects

---
+++ js/main.js
@@ -0,0 +1,13 @@
+$('#next-slide').click(function() {
+    var slide = $('#slideshow .slide').first();
+    $(slide).animate({
+        left: '-50%'
+    }, 500, function() {
+        $(slide).css('left', '150%');
+        $(slide).appendTo('#slideshow');
+    });
+
+    $('#slideshow .slide').next().animate({
+        left: '50%'
+    }, 500);
+});
